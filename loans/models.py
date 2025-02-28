d30b02796a7fad67177210cd37d81b71019455b1
onedream0824 <onedream082498@gmail.com>
2025-02-28 09:05:14 -0800

    init commit

---
+++ loans/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users.models import User
+from books.models import Book
+
+class Loan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    returned_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} borrowed {self.book.title}"
